da8bbca000176ae7f379fd38cd3d044cdd20073f
Joseph A. Levin <josephalevin@gmail.com>
2011-03-15 22:28:09 -0500

    Skeleton files.  No logic yet.

---
+++ src/main/java/com/josephalevin/gwtplug/client/ExtensionLoader.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.josephalevin.gwtplug.client;
+
+/**
+ *
+ * @author josephalevin
+ */
+public interface ExtensionLoader <E> extends Iterable<E>{
+    
+}
