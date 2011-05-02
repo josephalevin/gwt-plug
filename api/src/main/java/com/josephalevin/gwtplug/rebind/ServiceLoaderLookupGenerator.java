a55e1493396df5000d7ac8774f0233d1e79e417c
Joseph A. Levin <josephalevin@gmail.com>
2011-05-02 02:18:50 -0500

    Removed unused imports.

--- api/src/main/java/com/josephalevin/gwtplug/rebind/ServiceLoaderLookupGenerator.java
+++ api/src/main/java/com/josephalevin/gwtplug/rebind/ServiceLoaderLookupGenerator.java
@@ -19,7 +19,6 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -27,7 +26,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.NoSuchElementException;
 
 /**
  *
